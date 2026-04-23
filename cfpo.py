9b03e42611262bab596610fd9f8c56b716ee672d
leykhan16 <adeyemiadebayo72@gmail.com>
2026-04-23 18:09:02 +0100

    section 3

---
+++ cfpo.py
@@ -0,0 +1,33 @@
+#Arithmetic Operators
+#+	Addition
+bill = 100
+tip = 20
+total = bill + tip
+print(total)
+#-	Subtraction
+difference = bill - tip
+print(difference)
+#*	Multiplication
+product = bill * tip
+print(product)
+#/	Division
+quotient = bill / tip
+print(quotient)
+#%	Modulus
+remainder = bill % tip
+print(remainder)
+#**	Exponentiation
+power = bill ** tip
+print(power)
+#//	Floor division
+floor_division = bill // tip
+print(floor_division)
+#modulus division
+modulus_division = bill % tip
+print(modulus_division)
+#modulus remainder
+modulus_remainder = bill % tip
+print(modulus_remainder)
+#modulo operator
+modulo_result = bill % tip
+print(modulo_result)
