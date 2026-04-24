3f69911550ead94adc6c8cf8245230b0449abd64
leykhan16 <adeyemiadebayo72@gmail.com>
2026-04-24 18:37:21 +0100

    section 4

---
+++ section4/bank.py
@@ -0,0 +1,9 @@
+all_users = {"john", "luke", "philip"}
+def register_user(name, id):
+    all_users.append(name)
+
+def delete_user(name, id):
+    all_users.remove(name)
+
+def total_user():
+    return len(all_users)
---
+++ section4/main.py
@@ -0,0 +1,5 @@
+from bank import all_users as alu
+
+all_users = {"michael", "sarah", "emma"}
+print(all_users)
+print(alu)
